4d08e0f6fbc845c5d6795408bbd3d9e6f2f2c704
Waseem Khalil <waseemkhalilworld@gmail.com>
2024-11-27 03:10:39 -0800

    2nd commit

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -72,7 +72,7 @@ const Navbar = () => {
                 {/* left  */}
                 <div className="left py-3 lg:py-0">
                     <Link to={'/'}>
-                        <h2 className=" font-bold text-white text-2xl text-center">E-Bharat</h2>
+                        <h2 className=" font-bold text-white text-2xl text-center">Shoping Mall</h2>
                     </Link>
                 </div>
 
